85957e9039eb08aabb179f544733cef2ceba9e0e
Tim Berthold <tim.berthold@sovity.de>
2023-05-25 07:41:20 +0200

    feat: ConnectorQueue scheduler

--- extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/services/queue/ConnectorQueueJob.java
+++ extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/services/queue/ConnectorQueueJob.java
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (c) 2023 sovity GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       sovity GmbH - initial API and implementation
+ *
+ */
 package de.sovity.edc.ext.brokerserver.services.queue;
 
 import de.sovity.edc.ext.brokerserver.dao.queries.ConnectorQueries;
@@ -11,7 +24,7 @@ import org.quartz.JobExecutionException;
 import java.time.OffsetDateTime;
 
 @RequiredArgsConstructor
-public class ConnectorQueueScheduler implements Job {
+public class ConnectorQueueJob implements Job {
     private final DslContextFactory dslContextFactory;
 
     @Override
@@ -20,11 +33,11 @@ public class ConnectorQueueScheduler implements Job {
         var connectorQueue = (ConnectorQueue) context.get("connectorQueue");
 
         dslContextFactory.transaction(dsl -> {
-                var connectorRecords = connectorQueries.findAll(dsl);
-                connectorRecords.forEach(connectorRecord -> {
-                    var connectorQueueEntry = new ConnectorQueueEntry(connectorRecord.getEndpoint(), OffsetDateTime.now(), ConnectorRefreshPriority.SCHEDULED_REFRESH);
-                    connectorQueue.add(connectorQueueEntry);
-                });
+            var connectorRecords = connectorQueries.findAll(dsl);
+            connectorRecords.forEach(connectorRecord -> {
+                var connectorQueueEntry = new ConnectorQueueEntry(connectorRecord.getEndpoint(), OffsetDateTime.now(), ConnectorRefreshPriority.SCHEDULED_REFRESH);
+                connectorQueue.add(connectorQueueEntry);
+            });
         });
     }
 }
---
+++ extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/services/queue/ConnectorQueueJobFactory.java
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2023 sovity GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       sovity GmbH - initial API and implementation
+ *
+ */
+package de.sovity.edc.ext.brokerserver.services.queue;
+
+import org.quartz.Job;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+
+public class ConnectorQueueJobFactory implements JobFactory {
+    @Override
+    public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) throws SchedulerException {
+        return (Job) bundle.getJobDetail().getJobDataMap().get("connectorQueueJob");
+    }
+}
