d38707d004a8690700c404776c2482fbcc5138ad
Tim Berthold <tim.berthold@sovity.de>
2023-06-13 07:42:10 +0200

    chore: add license header where missing

--- extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/services/api/filtering/CatalogFilterAttributeDefinitionService.java
+++ extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/services/api/filtering/CatalogFilterAttributeDefinitionService.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2023 sovity GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       sovity GmbH - initial API and implementation
+ *
+ */
+
 package de.sovity.edc.ext.brokerserver.services.api.filtering;
 
 import de.sovity.edc.ext.brokerserver.dao.pages.catalog.CatalogQueryFields;
@@ -12,7 +26,7 @@ public class CatalogFilterAttributeDefinitionService {
         return new CatalogFilterAttributeDefinition(
                 assetProperty,
                 label,
-                (fields) -> getValue(fields, assetProperty),
+                fields -> getValue(fields, assetProperty),
                 (fields, values) -> PostgresqlUtils.in(getValue(fields, assetProperty), values)
         );
     }
