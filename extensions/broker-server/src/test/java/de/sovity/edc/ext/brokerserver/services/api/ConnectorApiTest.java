4c1a434b25c126f9e1382fe6d0bb0f40a1845a9d
Tim Berthold <tim.berthold@sovity.de>
2023-06-21 10:06:44 +0200

    refactor: minor test refactorings

--- extensions/broker-server/src/test/java/de/sovity/edc/ext/brokerserver/services/api/ConnectorApiTest.java
+++ extensions/broker-server/src/test/java/de/sovity/edc/ext/brokerserver/services/api/ConnectorApiTest.java
@@ -14,23 +14,33 @@
 
 package de.sovity.edc.ext.brokerserver.services.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.sovity.edc.client.gen.model.ConnectorDetailPageQuery;
 import de.sovity.edc.client.gen.model.ConnectorPageQuery;
-import de.sovity.edc.ext.brokerserver.BrokerServerExtension;
+import de.sovity.edc.ext.brokerserver.dao.AssetProperty;
 import de.sovity.edc.ext.brokerserver.db.TestDatabase;
 import de.sovity.edc.ext.brokerserver.db.TestDatabaseFactory;
+import de.sovity.edc.ext.brokerserver.db.jooq.Tables;
+import de.sovity.edc.ext.brokerserver.db.jooq.enums.ConnectorContractOffersExceeded;
+import de.sovity.edc.ext.brokerserver.db.jooq.enums.ConnectorDataOffersExceeded;
+import de.sovity.edc.ext.brokerserver.db.jooq.enums.ConnectorOnlineStatus;
+import lombok.SneakyThrows;
 import org.eclipse.edc.junit.annotations.ApiTest;
 import org.eclipse.edc.junit.extensions.EdcExtension;
+import org.eclipse.edc.policy.model.Policy;
+import org.jooq.DSLContext;
+import org.jooq.JSONB;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.time.OffsetDateTime;
 import java.util.Map;
 
-import static de.sovity.edc.client.gen.model.ConnectorListEntry.OnlineStatusEnum.OFFLINE;
 import static de.sovity.edc.ext.brokerserver.TestUtils.createConfiguration;
 import static de.sovity.edc.ext.brokerserver.TestUtils.edcClient;
+import static groovy.json.JsonOutput.toJson;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ApiTest
@@ -43,25 +53,92 @@ class ConnectorApiTest {
     @BeforeEach
     void setUp(EdcExtension extension) {
         extension.setConfiguration(createConfiguration(TEST_DATABASE, Map.of(
-                BrokerServerExtension.KNOWN_CONNECTORS, "https://example.com/ids/data"
         )));
     }
 
     @Test
     void testQueryConnectors() {
-        var result = edcClient().brokerServerApi().connectorPage(new ConnectorPageQuery());
-        assertThat(result.getConnectors()).hasSize(1);
+        TEST_DATABASE.testTransaction(dsl -> {
+            var today = OffsetDateTime.now().withNano(0);
 
-        var connector = result.getConnectors().get(0);
-        assertThat(connector.getEndpoint()).isEqualTo("https://example.com/ids/data");
-        assertThat(connector.getId()).isEqualTo("https://example.com");
-        assertThat(connector.getOnlineStatus()).isEqualTo(OFFLINE);
+            createConnector(dsl, today, "http://my-connector/ids/data");
+            createDataOffer(dsl, today, Map.of(
+                AssetProperty.ASSET_ID, "urn:artifact:my-asset-1",
+                AssetProperty.DATA_CATEGORY, "my-category",
+                AssetProperty.ASSET_NAME, "My Asset 1"
+            ), "http://my-connector/ids/data");
+
+            var result = edcClient().brokerServerApi().connectorPage(new ConnectorPageQuery());
+            assertThat(result.getConnectors()).hasSize(1);
+
+            var connector = result.getConnectors().get(0);
+            assertThat(connector.getEndpoint()).isEqualTo("http://my-connector/ids/data");
+            assertThat(connector.getId()).isEqualTo("http://my-connector");
+        });
     }
 
     @Test
     void testQueryConnectorDetails() {
-        var connector = edcClient().brokerServerApi().connectorDetailPage(new ConnectorDetailPageQuery("https://example.com/ids/data"));
-        assertThat(connector.getEndpoint()).isEqualTo("https://example.com/ids/data");
-        assertThat(connector.getId()).isEqualTo("https://example.com");
+        TEST_DATABASE.testTransaction(dsl -> {
+            var today = OffsetDateTime.now().withNano(0);
+
+            createConnector(dsl, today, "http://my-connector/ids/data");
+            createDataOffer(dsl, today, Map.of(
+                AssetProperty.ASSET_ID, "urn:artifact:my-asset-1",
+                AssetProperty.DATA_CATEGORY, "my-category",
+                AssetProperty.ASSET_NAME, "My Asset 1"
+            ), "http://my-connector/ids/data");
+
+            var connector = edcClient().brokerServerApi().connectorDetailPage(new ConnectorDetailPageQuery("https://example.com/ids/data"));
+            assertThat(connector.getEndpoint()).isEqualTo("https://example.com/ids/data");
+            assertThat(connector.getId()).isEqualTo("https://example.com");
+        });
+    }
+
+    private void createConnector(DSLContext dsl, OffsetDateTime today, String connectorEndpoint) {
+        var connector = dsl.newRecord(Tables.CONNECTOR);
+        connector.setConnectorId("http://my-connector");
+        connector.setEndpoint(connectorEndpoint);
+        connector.setOnlineStatus(ConnectorOnlineStatus.ONLINE);
+        connector.setCreatedAt(today.minusDays(1));
+        connector.setLastRefreshAttemptAt(today);
+        connector.setLastSuccessfulRefreshAt(today);
+        connector.setDataOffersExceeded(ConnectorDataOffersExceeded.OK);
+        connector.setContractOffersExceeded(ConnectorContractOffersExceeded.OK);
+        connector.insert();
+    }
+
+    private void createDataOffer(DSLContext dsl, OffsetDateTime today, Map<String, String> assetProperties, String connectorEndpoint) {
+        var dataOffer = dsl.newRecord(Tables.DATA_OFFER);
+        dataOffer.setAssetId(assetProperties.get(AssetProperty.ASSET_ID));
+        dataOffer.setAssetName(assetProperties.getOrDefault(AssetProperty.ASSET_NAME, dataOffer.getAssetId()));
+        dataOffer.setAssetProperties(JSONB.jsonb(assetProperties(assetProperties)));
+        dataOffer.setConnectorEndpoint(connectorEndpoint);
+        dataOffer.setCreatedAt(today.minusDays(5));
+        dataOffer.setUpdatedAt(today);
+        dataOffer.insert();
+
+        var contractOffer = dsl.newRecord(Tables.DATA_OFFER_CONTRACT_OFFER);
+        contractOffer.setContractOfferId("my-contract-offer-1");
+        contractOffer.setConnectorEndpoint(connectorEndpoint);
+        contractOffer.setAssetId(assetProperties.get(AssetProperty.ASSET_ID));
+        contractOffer.setCreatedAt(today.minusDays(5));
+        contractOffer.setUpdatedAt(today);
+        contractOffer.setPolicy(JSONB.jsonb(policyToJson(dummyPolicy())));
+        contractOffer.insert();
+    }
+    private Policy dummyPolicy() {
+        return Policy.Builder.newInstance()
+            .assignee("Example Assignee")
+            .build();
+    }
+
+    private String policyToJson(Policy policy) {
+        return toJson(policy);
+    }
+
+    @SneakyThrows
+    private String assetProperties(Map<String, String> assetProperties) {
+        return new ObjectMapper().writeValueAsString(assetProperties);
     }
 }
