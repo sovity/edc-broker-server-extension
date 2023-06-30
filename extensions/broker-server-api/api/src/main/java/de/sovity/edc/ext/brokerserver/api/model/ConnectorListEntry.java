1f92b8197445eaa91e143137a28045783db65641
Tim Berthold <tim.berthold@sovity.de>
2023-06-30 14:15:53 +0200

    feat: Display Connector Crawling Time

--- extensions/broker-server-api/api/src/main/java/de/sovity/edc/ext/brokerserver/api/model/ConnectorListEntry.java
+++ extensions/broker-server-api/api/src/main/java/de/sovity/edc/ext/brokerserver/api/model/ConnectorListEntry.java
@@ -50,5 +50,14 @@ public class ConnectorListEntry {
 
     @Schema(description = "Number of known data offerings")
     private Integer numContractOffers;
+
+    @Schema(description = "Minimum duration of crawling process in milliseconds")
+    private long connectorCrawlingTimeMin;
+
+    @Schema(description = "Maximum duration of crawling process in milliseconds")
+    private long connectorCrawlingTimeMax;
+
+    @Schema(description = "Average duration of crawling process in milliseconds")
+    private long connectorCrawlingTimeAvg;
 }
 
