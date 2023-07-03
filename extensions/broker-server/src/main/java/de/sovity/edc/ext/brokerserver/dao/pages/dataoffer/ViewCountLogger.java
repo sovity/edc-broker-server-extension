7b6417a71f117abd9cea8343d859721e2e0b43f4
Tim Berthold <tim.berthold@sovity.de>
2023-07-03 09:34:07 +0200

    refactor: pr remarks

--- extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/dao/pages/dataoffer/ViewCountLogger.java
+++ extensions/broker-server/src/main/java/de/sovity/edc/ext/brokerserver/dao/pages/dataoffer/ViewCountLogger.java
@@ -8,9 +8,6 @@ import java.time.OffsetDateTime;
 public class ViewCountLogger {
     public void increaseDataOfferViewCount(DSLContext dsl, String assetId, String endpoint) {
         var v = Tables.DATA_OFFER_VIEW_COUNT;
-
-        dsl.insertInto(v, v.ASSET_ID, v.CONNECTOR_ENDPOINT, v.DATE)
-            .values(assetId, endpoint, OffsetDateTime.now())
-            .execute();
+        dsl.insertInto(v, v.ASSET_ID, v.CONNECTOR_ENDPOINT, v.DATE).values(assetId, endpoint, OffsetDateTime.now()).execute();
     }
 }
