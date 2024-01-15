6dca8770551ad8a6c6dbe97b66fddce4e907c2b7
Jan Ridderbusch <kontakt@jan-ridderbusch.de>
2024-01-15 15:08:03 +0100

    fix: TS library generation

--- extensions/broker-server-api/api/src/main/java/de/sovity/edc/ext/brokerserver/api/model/AuthorityPortalOrganizationMetadata.java
+++ extensions/broker-server-api/api/src/main/java/de/sovity/edc/ext/brokerserver/api/model/AuthorityPortalOrganizationMetadata.java
@@ -14,7 +14,7 @@ import java.time.OffsetDateTime;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "Information about organizations from the Authority Portal.", requiredMode = Schema.RequiredMode.REQUIRED)
+@Schema(description = "Information about organizations from the Authority Portal.")
 public class AuthorityPortalOrganizationMetadata {
     @Schema(description = "MDS-ID from the Authority Portal", requiredMode = Schema.RequiredMode.REQUIRED)
     private String mdsId;
